f46880150fc0aa1b4c7c6f92d47ab3d2664a907d
Aly Khaled <alykhaled936@gmail.com>
2024-04-25 00:36:00 +0200

    Upload Birthdaycard App

---
+++ birthdaycard/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BirthdayCard());
+}
+
+class BirthdayCard extends StatelessWidget {
+  const BirthdayCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        
+        body: Center(
+            child: Image(image: AssetImage("images/450w-CcRpQPqOVG0.webp"))),
+      ),
+    );
+  }
+}
